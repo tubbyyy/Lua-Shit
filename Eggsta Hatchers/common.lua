d4e4c794749e5b20eae6ba2294779e283e7cc6c9
tubbyyy <williams@roycemoreschool.org>
2023-06-17 14:05:53 -0500

    f

--- Eggsta Hatchers/common.lua
+++ Eggsta Hatchers/common.lua
@@ -1,5 +1,7 @@
 common = {}
 
+common.__index = common
+
 function common:GetTime(Point1, Point2, Speed)
     local Distance = (Point1.Position - Point2.Position).Magnitude
     local Time = Distance / Speed
